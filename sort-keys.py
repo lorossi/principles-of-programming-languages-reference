f1e10e13a33ff9fa1cc8545f43b5922079888298
Lorenzo Rossi <mail@lorenzoros.si>
2024-01-07 11:52:12 +0100

    made code clearer

--- sort-keys.py
+++ sort-keys.py
@@ -4,7 +4,8 @@ various languages in lstlisting (see settings.sty).
 
 All the keywords are TEMPORARILY stored in a file called "keywords"; \
 another file can be specified as argument.
-That same file will be overwritten with the sorted keywords.
+After sorting, the keywords are stored in a file called "sorted-keywords" \
+(or "sorted-<file_name>" if another file is specified as argument).
 """
 
 import sys
@@ -12,8 +13,12 @@ import sys
 
 def main(file_name: str):
     """Script entry point."""
-    with open(file_name) as f:
-        data = f.read()
+    try:
+        with open(file_name) as f:
+            data = f.read()
+    except FileNotFoundError:
+        print(f"File {file_name} not found.")
+        return
 
     keywords = data.replace("\n", ",").split(",")
     unique_keywords = set(k.strip() for k in keywords)
@@ -21,12 +26,17 @@ def main(file_name: str):
     sorted_keywords = sorted(sorted_keywords, key=str.lower)
     keywords_str = ", ".join(sorted_keywords) + ","
 
-    with open(file_name, "w") as f:
+    out_file_name = f"sorted-{file_name}"
+    with open(out_file_name, "w") as f:
         f.write(keywords_str)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
+        print(
+            "Sorting keywords in file 'keywords'. To specify another file, "
+            "use 'python sort-keys.py <file_name>'"
+        )
         main("keywords")
     else:
         main(sys.argv[1])
